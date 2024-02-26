aacd18d60efecd5398779e1d9d19f71b29856a9f
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-02-26 11:00:40 +0300

    Create 04-degismezler.rs

---
+++ 04-degismezler.rs
@@ -0,0 +1,21 @@
+#[allow(unused_variables)]
+
+
+fn main() {
+
+    //BENI_BOYLE_TANIMLA
+    //const sözcüğünü unutmayalım!
+    
+    const PI: f32 = 3.141;
+    
+    const BEN_KIMIM: &str = "Mustafa Büyükdereli";
+    
+    println!("Pi sayısı ve ben değişmeyiz: {} <---> {}", PI, BEN_KIMIM);
+
+}
+
+
+/*
+Pi sayısı ve ben değişmeyiz: 3.141 <---> Mustafa Büyükdereli
+*/
+
