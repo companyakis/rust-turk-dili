710cbc9b0caf9f76f93e29b002084e771f703b55
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-02-26 09:56:44 +0300

    Create 01-degiskenler2.rs

---
+++ 01-degiskenler2.rs
@@ -0,0 +1,25 @@
+#[allow(unused_variables)]
+
+
+fn main() {
+
+    //değiştirilebilir değişken tanımlayalım
+    
+    let mut yil: u16 = 1997;
+    
+    let dogum_yili: u16 = 1970;
+    
+    println!("Ali'nin yaşı: {}", yil - dogum_yili);
+    
+    yil = 2024;
+    
+    println!("Ali'nin yaşı: {}", yil - dogum_yili);
+    
+}
+
+
+/*
+Ali'nin yaşı: 27
+Ali'nin yaşı: 54
+*/
+
