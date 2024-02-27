3dc90eb2093e49a00122e0ae5c14ddeb8b8654f4
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-02-27 19:44:44 +0300

    Create 08-donguler3-for.rs

---
+++ 08-donguler3-for.rs
@@ -0,0 +1,25 @@
+fn main() {
+
+    //.. ile kullanımda, son sayı dahil değil!
+    
+    //0-10 arası sayıları yazdıralım
+  
+    for i in 0..11 {
+        println!("Sayı_{}: {}", i + 1, i);
+    }
+
+}
+
+/*
+Sayı_1: 0
+Sayı_2: 1
+Sayı_3: 2
+Sayı_4: 3
+Sayı_5: 4
+Sayı_6: 5
+Sayı_7: 6
+Sayı_8: 7
+Sayı_9: 8
+Sayı_10: 9
+Sayı_11: 10
+*/
