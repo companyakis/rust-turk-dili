fa73a54bf454674e7f3d946b75beadd1be4cba89
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-02-28 12:34:40 +0300

    Create 05-diziler2.rs

---
+++ 05-diziler2.rs
@@ -0,0 +1,14 @@
+fn main() {
+
+    //aynı elemanlardan oluşan dizi tanımlayabiliriz
+
+    let a = [0; 8];
+
+    println!("{:?}", a); // [0, 0, 0, 0, 0, 0, 0, 0]
+
+    let b = ["Mustafa"; 5];
+
+    println!("{:?}", b); // ["Mustafa", "Mustafa", "Mustafa", "Mustafa", "Mustafa"]
+
+}
+
