7cc9daec9f152899c91b8780889116b984102416
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-03-04 09:56:00 +0300

    Create 07-kararyapilari2.rs

---
+++ 07-kararyapilari2.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let sayi1: i8 = 15;
+
+    let sayi2: i8 = -33;
+
+    //let sayi3: i8 = 0;
+
+    let en_buyuk_sayi = if sayi1 > sayi2 { sayi1 } else { sayi2 };
+
+    println!("En büyük sayı: {}", en_buyuk_sayi); //En büyük sayı: 15
+}
