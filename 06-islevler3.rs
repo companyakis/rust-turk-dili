5873893adf2f9a5fafdc215bbcf948f13cc4ed91
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-03-05 10:32:28 +0300

    Create 06-islevler3.rs

---
+++ 06-islevler3.rs
@@ -0,0 +1,19 @@
+fn yazdir_islevi(sayi: u8, yazi: String) {
+  
+    println!("Sayı: {} ve yazı: {}", sayi, yazi);
+}
+
+fn main() {
+
+    //kullanıcı tanımlı işlev ve can sıkıcı sahiplik kuralları için bir örnek
+
+    let sayi: u8 = 101;
+
+    let ben: String = "Mustafa Büyükdereli".to_string();
+
+    yazdir_islevi(sayi, ben); //Sayı: 101 ve yazı: Mustafa Büyükdereli
+
+    //println!("{}", ben); //hata alırız! ben değişkeninin değeri yazi'ya taşındı
+
+    //clone() kullanmak istiyorum:)
+}
