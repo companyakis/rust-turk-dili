acb261f985d59c14ba1b30e4dc22c8096aec5de5
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-03-12 00:18:35 +0300

    Create 14-vectors4.rs

---
+++ 14-vectors4.rs
@@ -0,0 +1,14 @@
+fn main() {
+
+    //vector öğelerini yazdıralım
+
+    let sayilar: Vec<i16> = vec![2024, 5000, 2478];
+
+    for sayi in sayilar {
+        print!("{sayi} ");
+    }
+
+}
+
+//2024 -5000 2478 
+
